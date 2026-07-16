40bf6eb49cac1f61f1b67f37f1ad80ef70932700
echoesofsoulprueba-ux <echoesofsoulprueba@gmail.com>
2026-07-16 17:09:22 -0300

    chore: add design system to design-system/

---
+++ design-system/tailwind-preset.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      "colors": {
+        "card": "#F7F4F0",
+        "error": "#B32D2D",
+        "muted": "#EBEAE8",
+        "border": "#D4D1CC",
+        "sidebar": "#8C572D",
+        "success": "#1E6C1E",
+        "warning": "#F0C000",
+        "secondary": "#459A9A",
+        "background": "#FCFBF9",
+        "foreground": "#1F2937",
+        "sidebar-active": "#A65E26",
+        "card-foreground": "#1F2937",
+        "error-foreground": "#FFFFFF",
+        "muted-foreground": "#454C54",
+        "primary-foreground": "#1F2937",
+        "sidebar-foreground": "#FFFFFF",
+        "success-foreground": "#FFFFFF",
+        "warning-foreground": "#1F2937",
+        "secondary-foreground": "#000000",
+        "sidebar-active-foreground": "#FFFFFF",
+        "accent": {
+          "50": "#f6f9f9",
+          "100": "#ecf3f3",
+          "200": "#d4e7e7",
+          "300": "#b5d9d9",
+          "400": "#89c8c8",
+          "500": "#61b8b8",
+          "600": "#49a2a2",
+          "700": "#3e8484",
+          "800": "#326767",
+          "900": "#224444"
+        },
+        "neutral": {
+          "50": "#f7f7f8",
+          "100": "#eff0f0",
+          "200": "#dcdee0",
+          "300": "#c3c6cb",
+          "400": "#a2a7af",
+          "500": "#838b96",
+          "600": "#6c747f",
+          "700": "#596068",
+          "800": "#474c52",
+          "900": "#2f3237"
+        },
+        "primary": {
+          "50": "#faf7f4",
+          "100": "#f7efe9",
+          "200": "#f1dccb",
+          "300": "#ecc4a2",
+          "400": "#e8a369",
+          "500": "#e58434",
+          "600": "#d06d1b",
+          "700": "#a85b1a",
+          "800": "#824817",
+          "900": "#543012",
+          "DEFAULT": "#E68A3D"
+        }
+      },
+      "fontFamily": {
+        "sans": [
+          "Montserrat",
+          "sans-serif"
+        ]
+      }
+    },
+  },
+}
